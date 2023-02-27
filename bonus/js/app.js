552b529e038b49d238772320a9df4150c2ef97c0
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-02-27 15:45:23 +0100

    Bonus added

---
+++ bonus/js/app.js
@@ -0,0 +1,27 @@
+
+
+const { createApp } = Vue
+
+
+
+
+  createApp({
+    data() {
+      return {
+        allEmail : [],
+      }
+    },
+    mounted() {
+ 
+      for (let i = 0; i < 10; i++) {
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res) => {
+
+        const email = res.data.response;
+        this.allEmail.push(email)
+
+        })
+      }
+    }
+  }).mount('#app')
